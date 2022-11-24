851499a938c6d7efe93b7a9bbd0eb885e2a4fb80
thaii dinhh <DHT_arduino@hotmail.com>
2022-11-24 22:38:57 +0700

    Add project files.

---
+++ Ping Sweep Demo/FormMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FindDevice
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSweepAsync = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSweepAsync
+            // 
+            this.buttonSweepAsync.Location = new System.Drawing.Point(436, 7);
+            this.buttonSweepAsync.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSweepAsync.Name = "buttonSweepAsync";
+            this.buttonSweepAsync.Size = new System.Drawing.Size(107, 43);
+            this.buttonSweepAsync.TabIndex = 1;
+            this.buttonSweepAsync.Text = "Ping find device";
+            this.buttonSweepAsync.UseVisualStyleBackColor = true;
+            this.buttonSweepAsync.Click += new System.EventHandler(this.buttonSweepAsync_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 115);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(531, 73);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Range IP:";
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(109, 7);
+            this.txt_ip.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(129, 22);
+            this.txt_ip.TabIndex = 4;
+            this.txt_ip.Text = "192.168.1";
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(13, 192);
+            this.lblresult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(48, 17);
+            this.lblresult.TabIndex = 5;
+            this.lblresult.Text = "Result";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 69);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 51);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Set";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "IP NEW";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(258, 69);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(143, 24);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.Visible = false;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IP hiện tại : ";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "IP mới: ";
+            this.label4.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(552, 211);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.txt_ip);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonSweepAsync);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "setID_DMX";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonSweepAsync;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.Label lblresult;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Ping Sweep Demo/FormMain.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindDevice
+{
+    public partial class FormMain : Form
+    {       
+        private string BaseIP = "172.16.0.";
+        private int StartIP = 2;
+        private int StopIP = 255;
+        private string ip;
+        private string list_ip;
+        private int timeout = 3000;
+        private int nFound = 0;
+       
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        static object lockObj = new object();
+        Stopwatch stopWatch = new Stopwatch();
+        TimeSpan ts;
+        public FormMain()
+        {
+            InitializeComponent();
+            string[] BaudRate = { "101", "102", "103", "104", "105", "106", "107", "108", "109", "110" };
+            comboBox2.Items.AddRange(BaudRate);//  
+        }
+
+        private void buttonSweepAsync_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            RunPingSweep_Async(txt_ip.Text);          
+        }
+        public async void RunPingSweep_Async(string txt_ip)
+        {
+            nFound = 0;
+            list_ip = "";
+            BaseIP = txt_ip + ".";
+            var tasks = new List<Task>();
+
+            stopWatch.Start();
+
+            for (int i = StartIP; i <= StopIP; i++)
+            {
+                ip = BaseIP + i.ToString();
+
+                System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+                var task = PingAndUpdateAsync(p, ip);
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks).ContinueWith(t =>
+            {
+                stopWatch.Stop();
+                ts = stopWatch.Elapsed;
+             
+                richTextBox1.BeginInvoke(new Action(() =>
+                {
+
+                    label3.Visible = true;
+                    label4.Visible = true;
+                    comboBox1.Visible = true;
+                    comboBox2.Visible = true;
+                    //richTextBox1.Text = list_ip;
+                    //comboBox1.Items.Add(list_ip);
+                    //list_ip = "";
+                    lblresult.Text = nFound.ToString() + " thiết bị được tìm thấy! Elapsed time: " + ts.ToString();
+                }));
+                
+            });
+        }
+        
+
+        private async Task PingAndUpdateAsync(System.Net.NetworkInformation.Ping ping, string ip)
+        {
+            var reply = await ping.SendPingAsync(ip, timeout);
+
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                lock (lockObj)
+                {
+                    list_ip += ip + Environment.NewLine;
+                    comboBox1.Items.Add(ip);
+                    nFound++;
+                }
+            }
+        }
+        String ip_select;
+     
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ip_select = comboBox1.SelectedItem.ToString();
+
+        }
+        String ip_cuoi;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label2.Text = ip_select;
+            IPAddress broadcast = IPAddress.Parse(ip_select);
+            byte[] sendbuf = Encoding.ASCII.GetBytes("?Setaddress^"+ (Int32.Parse(ip_cuoi)-100) + "*");
+            IPEndPoint ep = new IPEndPoint(broadcast, 96);
+            s.SendTo(sendbuf, ep);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            lblresult.Visible = true;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ip_cuoi = comboBox2.SelectedItem.ToString();
+            label2.Text = txt_ip.Text+"." + ip_cuoi;
+            foreach (String khachHang in comboBox1.Items)
+            {
+                // Thực thi phần in hóa đơn của từng khách hàng
+               if(khachHang == label2.Text)
+                {
+                    
+                    label2.Text = "Bị trùng IP";
+                    label2.BackColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+    }
+
+    
+}
